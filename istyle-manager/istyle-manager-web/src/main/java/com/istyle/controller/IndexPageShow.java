4e6f2f4e08e46ec8132ccc15fb2de63f39abbd03
Beginner-hww <15217821903@163.com>
2018-04-03 22:04:02 +0800

    ssm框架和首页展示

---
+++ istyle-manager/istyle-manager-web/src/main/java/com/istyle/controller/IndexPageShow.java
@@ -0,0 +1,22 @@
+package com.istyle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页展示
+ * by 黄文伟
+ * 2018年4月3日10:13:02
+ */
+
+@Controller
+public class IndexPageShow {
+    @RequestMapping("/")
+    public String showIndex() {
+        System.out.println("a");
+        return "index";
+    }
+/*    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){return page;}*/
+}
